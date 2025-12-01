3911adbc87dbe0e046d2c9e3671683b9aa230def
Gicely <2024222@student.cct.ie>
2025-12-01 10:55:42 +0000

    updates

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db");
 const app = express();
+const fs = require("fs");
+const csv = require("csv-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,3 +21,31 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
+function validateRow(row, lineNumber) {
+    if (!row.first_name || row.first_name.length > 20) return `Error in row ${lineNumber}`;
+    return null;
+}
+
+app.get("/upload-csv", (req, res) => {
+    const validRows = [];
+    let line = 1;
+
+    fs.createReadStream("/mnt/data/person_info.csv")
+        .pipe(csv())
+        .on("data", (row) => {
+            const error = validateRow(row, line);
+            if (error) console.log(error);
+            else validRows.push(row);
+            line++;
+        })
+        .on("end", () => {
+            validRows.forEach(r => {
+                db.query(
+                    "INSERT INTO mysql_table (first_name, second_name, email, phone, eircode) VALUES (?,?,?,?,?)",
+                    [r.first_name, r.second_name, r.email, r.phone, r.eircode]
+                );
+            });
+            res.send("CSV processed");
+        });
+});
